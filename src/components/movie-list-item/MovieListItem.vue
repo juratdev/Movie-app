5d5b8c1a04ce292378943aa763dd8b26e90e92c4
juratdev <juratdev@gmail.com>
2023-07-23 23:02:29 +0500

    New style add

--- src/components/movie-list-item/MovieListItem.vue
+++ src/components/movie-list-item/MovieListItem.vue
@@ -5,7 +5,22 @@
 </script>
 
 <template>
-  <li>Empire of osman</li>
+  <li class="flex justify-between px-4 py-2 list-none border-b border-gray-200 rounded-t-lg dark:border-gray-400">
+    <span>Omar</span>
+    <input type="number" defaultValue="811">
+
+    <div class="flex items-center justify-center">
+      <button type="button">
+        <i class="fas fa-cookie"></i>
+      </button>
+
+      <button type="button">
+        <i class="fas fa-trash"></i>
+      </button>
+      
+      <i class="fas fa-star"></i>
+    </div>
+  </li>
 </template>
 
 <style scoped>
--- src/components/movie-list/MovieList.vue
+++ src/components/movie-list/MovieList.vue
@@ -8,7 +8,7 @@ import MovieListItem from '../movie-list-item/MovieListItem.vue'
 </script>
 
 <template>
-<ul class="p-6 mt-8 list-disc list-inside rounded shadow-xl bg-orange-50" role="list">
+<ul class="p-6 mt-8 list-disc list-inside rounded shadow-xl bg-orange-50 " role="list">
   <MovieListItem />
   <MovieListItem />
   <MovieListItem />
