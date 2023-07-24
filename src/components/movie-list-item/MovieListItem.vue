ce6dcde8b28d99bee9d25b8ebcddabb0f5f362a3
juratdev <juratdev@gmail.com>
2023-07-24 23:20:16 +0500

    New function and style add

--- src/components/movie-list-item/MovieListItem.vue
+++ src/components/movie-list-item/MovieListItem.vue
@@ -1,28 +1,46 @@
 <script>
   export default {
-
+    props: {
+      movie: {
+        type: Object,
+        required: true,
+      }
+    }
   }
 </script>
 
 <template>
-  <li class="flex justify-between px-4 py-2 list-none border-b border-gray-200 rounded-t-lg dark:border-gray-400">
-    <span>Omar</span>
-    <input type="number" defaultValue="811">
+  <li class="flex justify-between px-4 py-3 list-none border-b border-gray-500 rounded-t-lg list-group-item "
+      :class="[{like: movie.like},{favorite: movie.favorite}]"
+  >
+    <span class="text-2xl cursor-pointer w-[34.375rem] list-group-item-label">{{movie.name}}</span>
+    <input type="number" class="w-48 text-2xl text-center border-0 outline-none list-group-item-input" v-bind:value="movie.viewers">
 
     <div class="flex items-center justify-center">
-      <button type="button">
+      <button type="button" class="ml-2 text-lg text-orange-400 cursor-pointer w-9 h-9">
         <i class="fas fa-cookie"></i>
       </button>
 
-      <button type="button">
+      <button type="button" class="text-lg text-red-600 cursor-pointer w-9 h-9">
         <i class="fas fa-trash"></i>
       </button>
       
-      <i class="fas fa-star"></i>
+      <i class="mt-2 text-base text-center text-yellow-400 transition-all duration-300 translate-x-6 opacity-0 cursor-pointer fas fa-star w-9 h-9 "></i>
     </div>
   </li>
 </template>
 
 <style scoped>
-
+.list-group-item:last-child {
+  border-bottom: none;
+}
+.list-group-item.like .fa-star {
+  opacity: 1;
+  transform: translateX(0);
+}
+.list-group-item.favorite .list-group-item-label,
+.list-group-item.favorite .list-group-item-input
+{
+  color: #e09f3e;
+}
 </style>
--- src/components/movie-list/MovieList.vue
+++ src/components/movie-list/MovieList.vue
@@ -3,15 +3,37 @@ import MovieListItem from '../movie-list-item/MovieListItem.vue'
   export default {
     components: {
       MovieListItem
-    }
+    },
+    data() {
+      return {
+        movies: [
+          {
+            name: "Omar",
+            viewers: 811,
+            favorite: false,
+            like: true,
+          },
+          {
+            name: "Empire of osman",
+            viewers: 415,
+            favorite: false,
+            like: false,
+          },
+          {
+            name: "Ertugrul",
+            viewers: 708,
+            favorite: true,
+            like: false,
+          },
+        ]
+      }
+    },
   }
 </script>
 
 <template>
 <ul class="p-6 mt-8 list-disc list-inside rounded shadow-xl bg-orange-50 " role="list">
-  <MovieListItem />
-  <MovieListItem />
-  <MovieListItem />
+  <MovieListItem v-for="movie in movies" v-bind:movie="movie" />
 </ul>
 </template>
 
